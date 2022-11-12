069b3afc4eceeb3ad52b4b0fb5cc2cbce20f1925
FoxMD <ryusei@seznam.cz>
2022-11-12 18:49:42 +0100

    JSON lib

--- App/main.cpp
+++ App/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+#include "config.hpp"
+
 int main()
 {
+    std::cout << project_name <<std::endl;
+    std::cout << "JSON Lib version" << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "." << NLOHMANN_JSON_VERSION_PATCH << "." << std::endl;
     return 0;
 }
