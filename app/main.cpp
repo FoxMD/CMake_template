2a0919a567f20b39fa0bfb4235642ddc015f9faa
FoxMD <ryusei@seznam.cz>
2022-11-13 15:40:45 +0100

    spdlog and fmt for debug

--- app/main.cpp
+++ app/main.cpp
@@ -2,10 +2,15 @@
 
 #include <nlohmann/json.hpp>
 #include "config.hpp"
+#include <spdlog/spdlog.h>
+#include <fmt/format.h>
 
 int main()
 {
     std::cout << project_name <<std::endl;
     std::cout << "JSON Lib version" << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "." << NLOHMANN_JSON_VERSION_PATCH << "." << std::endl;
+    
+    spdlog::info(fmt::format("Welcome to {} v{}\n", project_name, project_version));
+    
     return 0;
 }
